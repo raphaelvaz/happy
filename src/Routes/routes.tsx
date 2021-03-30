c6f25ba3fc8846837f8adcdee47d9113ab9f9971
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-30 12:26:37 -0300

    feat: ensure that private routes can only be accessed by authorized users

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import Landing from './pages/Landing';
-import OrphanagesMap from './pages/OrphanagesMap';
-import Orphanage from './pages/Orphanage';
-import CreateOrphanage from './pages/CreateOrphanage';
-import OnSuccess from './pages/OnSuccess'
-import Login from './pages/Login'
+import { BrowserRouter, Switch } from 'react-router-dom';
+import Route from './Route'
+import Landing from '../pages/Landing';
+import OrphanagesMap from '../pages/OrphanagesMap';
+import Orphanage from '../pages/Orphanage';
+import CreateOrphanage from '../pages/CreateOrphanage';
+import OnSuccess from '../pages/OnSuccess'
+import Login from '../pages/Login'
+import OnDelete from '../pages/OnDelete'
 
 function Routes() {
     return (
@@ -19,6 +20,8 @@ function Routes() {
                 <Route path="/orphanages/:id" component={Orphanage} />
                 <Route path="/success" component={OnSuccess} />
                 <Route path="/login" component={Login} />
+
+                <Route isPrivate path="/deleted" component={OnDelete} />
             </Switch>
         </BrowserRouter>
     );
