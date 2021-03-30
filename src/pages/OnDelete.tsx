f66d7a59365c948cb2128fcfbe4b5375bc49b930
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-30 12:22:23 -0300

    feat: add onDelete page

---
+++ src/pages/OnDelete.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../styles/pages/on-delete.css';
+
+function onDelete() {
+
+    return (
+        <div id="page-onsuccess">
+            <div className="wrapper">
+                <main>
+                    <div>
+                        <h1>Excluir!</h1>
+                        <p>Você acabou de excluir um orfanato do nosso banco de dados.</p>
+                    </div>
+                    <Link to="/app">
+                        Voltar para o mapa
+                    </Link>
+                </main>
+            </div>
+        </div>
+    );
+}
+
+export default onDelete;
