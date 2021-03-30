29922279f0bc662095232c34c6556190cccfaa5b
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-30 16:47:51 -0300

    refactor: handleclick button from sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
 import { useHistory } from 'react-router-dom';
+import { useAuth } from '../hooks/auth';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/components/sidebar.css';
 
 const Sidebar: React.FC = ({ children }) => {
-    const { goBack } = useHistory();
+    const { goBack, location } = useHistory();
+    const { signOut } = useAuth()
 
+    const handleClick = () => {
+        if (location.pathname === '/dashboard') {
+            signOut()
+            return
+        }
+
+        goBack()
+        return
+    }
     return (
         <aside className="app-sidebar">
             <img src={mapMarkerImg} alt="Happy" />
-            <main>
+            <div>
                 {children}
-            </main>
+            </div>
             <footer>
-                <button type="button" onClick={goBack}>
+                <button type="button" onClick={handleClick}>
                     <FiArrowLeft size={24} color="#FFF" />
                 </button>
             </footer>
