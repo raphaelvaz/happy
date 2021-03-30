fd91bc2d39ec61014852808a6d2b39cf6f8ee640
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-30 12:29:12 -0300

    feat: sign in when user credentials are correct

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
-
+import { Link, useHistory } from 'react-router-dom';
+import { useAuth } from '../hooks/auth';
+import * as Yup from 'yup';
 import logoImg from '../images/logo-column.svg';
 import '../styles/pages/login.css';
 
 function Login() {
+    const { signIn } = useAuth()
+    const history = useHistory()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event: FormEvent): Promise<void> => {
+        event.preventDefault()
+        try {
+            const schema = Yup.object().shape({
+                email: Yup.string().required('Email obrigatório').email('Digite um email válido'),
+                password: Yup.string().required('Senha Obrigatória')
+            })
+
+            await schema.validate({ email, password });
+
+            await signIn({ email, password })
+            history.push('/dashboard')
+        } catch (err) {
+            console.log(err)
+            return;
+        }
+    }
 
     return (
         <div id="login">
@@ -18,15 +41,15 @@ function Login() {
             </div>
             <div className="form-block">
                 <Link to="/"><FiArrowLeft size={26} className="back-landing" /></Link>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h1>Fazer login</h1>
                     <div className="input-block">
                         <label htmlFor="email">E-mail</label>
-                        <input type="text" id="email" />
+                        <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" id="email" />
                     </div>
                     <div className="input-block">
                         <label htmlFor="senha">Senha</label>
-                        <input type="password" id="senha" />
+                        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="senha" />
                     </div>
                     <button className="confirm-button" type="submit">Entrar</button>
                 </form>
