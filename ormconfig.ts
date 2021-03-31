54652b1824b2a5db8e2f118c9cf2de46396123f1
raphaelvaz <raphaeltrombetta@gmail.com>
2021-03-31 15:25:56 -0300

    chore: change ormconfig file to dev environment

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,10 +2,12 @@ module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
     "migrations": [
-        "./dist/src/database/migrations/*.js"
+        // "./dist/src/database/migrations/*.js"
+        "./src/database/migrations/*.ts"
     ],
     "entities": [
-        "./dist/src/models/*.js"
+        // "./dist/src/database/migrations/*.js"
+        "./src/models/*.ts"
     ],
     "cli": {
         "migrationsDir": "./src/database/migrations"
